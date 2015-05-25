063a76a426a6c885262e83b0f65433772f3ba11d
Shadi Akiki <shadiakiki1986@gmail.com>
2015-05-25 14:58:38 +0300

    removed specifying the tls version

--- tests/dynamodbTest.php
+++ tests/dynamodbTest.php
@@ -16,7 +16,7 @@ var_dump(curl_version());
 	    'key' => getenv('J2P_AWS_KEY'), # check config file
 	    'secret'  => getenv('J2P_AWS_SECRET'),
 	    'region'  => getenv('J2P_AWS_REGION'),
-    'curl.options' => [CURLOPT_VERBOSE => true, CURLOPT_SSLVERSION=>CURL_SSLVERSION_TLSv1, CURLOPT_SSL_CIPHER_LIST => 'TLSv1']
+    'curl.options' => [CURLOPT_VERBOSE => true]
 	));
     }
 
