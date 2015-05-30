3d8de7d48c67fa91c5fd4739b427e1ee3d1c322f
Shadi Akiki <shadiakiki1986@gmail.com>
2015-05-30 18:39:21 +0300

    added download and build from source for gnutls so as to get 3.1.* + using travis cache to avoid having to recompile for each build

--- updateCurlOnUbuntu12.04.sh
+++ updateCurlOnUbuntu12.04.sh
@@ -12,6 +12,40 @@ if [ `lsb_release -a|grep Release|awk '{print $2}'` == '12.04' ]; then
 	sudo apt-get update
 	sudo apt-get -V build-dep curl libc6 libcurl3 libcurl3-gnutls php5-curl apache2 libgnutls28 libcurl4-gnutls-dev
 	sudo apt-get -V install curl libc6 libcurl3 libcurl3-gnutls php5-curl apache2 libgnutls28 libcurl4-gnutls-dev
+
+	# download and build from source: gnutls + nettle
+	if [ -d nettle-2.7.1 ]; then 
+		cd nettle-2.7.1
+		sudo make install
+		cd -
+	else
+		sudo apt-get install libgpm-dev autogen pkg-config
+		wget ftp://ftp.gnu.org/gnu/nettle/nettle-2.7.1.tar.gz
+		tar -xzf nettle-2.7.1.tar.gz
+		cd nettle-2.7.1
+		./configure
+		make
+		make check
+		sudo make install
+		cd -
+	fi
+
+	if [ -d gnutls-3.1.28 ]; then 
+		cd gnutls-3.1.28
+		sudo make install
+		cd -
+	else
+		wget ftp://ftp.gnutls.org/gcrypt/gnutls/stable/gnutls-3.1.28.tar.xz
+		unxz gnutls-3.1.28.tar.xz
+		tar -xvf gnutls-3.1.28.tar
+		cd gnutls-3.1.28
+		./configure
+		make
+		make check
+		sudo make install
+		cd -
+	fi
+
 else
 	echo "This script was only meant to run on Ubuntu 12.04";
 fi
