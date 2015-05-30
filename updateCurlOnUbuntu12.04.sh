8bf730c453cd73adf3a83391212a97cad7ca5027
Shadi Akiki <shadiakiki1986@gmail.com>
2015-05-30 11:47:23 +0300

    moved update curl code to separate script

---
+++ updateCurlOnUbuntu12.04.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Needed as described here
+# https://github.com/aws/aws-sdk-php/issues/588
+# Will no longer be needed when travis-ci start using Ubuntu 14.04
+
+echo 'deb http://security.ubuntu.com/ubuntu trusty main' > /tmp/myppa.list
+echo 'deb http://security.ubuntu.com/ubuntu trusty-security main' >> /tmp/myppa.list
+echo 'deb http://security.ubuntu.com/ubuntu trusty-updates main' >> /tmp/myppa.list
+sudo cp /tmp/myppa.list /etc/apt/sources.list.d/
+rm /tmp/myppa.list
+sudo apt-get update
+sudo apt-get -V build-dep curl libc6 libcurl3
+sudo apt-get -V install curl libc6 libcurl3
+
